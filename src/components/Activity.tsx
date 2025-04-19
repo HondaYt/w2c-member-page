609c3c24301308c672c912eb03951e96d078fc93
koudaihirata <2230051@ecc.ac.jp>
2025-04-20 02:15:11 +0900

    2025年度メンバー修正(新入生以外)

--- src/components/Activity.tsx
+++ src/components/Activity.tsx
@@ -3,37 +3,37 @@ import styles from "@/styles/components/activity.module.scss";
 import SectionTitle from "@/components/SectionTitle";
 
 export default function Activity() {
-	const isActive = true;
-	const ActivityDays = [
-		{ day: "日" },
-		{ day: "月", isActive },
-		{ day: "火", isActive },
-		{ day: "水" },
-		{ day: "木", isActive },
-		{ day: "金" },
-		{ day: "土" },
-	];
-	return (
-		<div id="activity" className={styles.activity}>
-			<SectionTitle title="活動日時" enTitle="activity" />
-			<div className={styles.activityDays}>
-				{ActivityDays.map((day) => (
-					<div
-						key={day.day}
-						className={`${styles.day} ${day.isActive ? styles.activeDay : ""}`}
-					>
-						{day.day}
-					</div>
-				))}
-			</div>
-			<div className={styles.activityTimes}>
-				<div className={styles.pie} />
-				<div className={styles.text}>
-					<span>放課後</span>から<span>閉館</span>まで
-					<br />
-					活動しています！
-				</div>
-			</div>
-		</div>
-	);
+    const isActive = true;
+    const ActivityDays = [
+        { day: "日" },
+        { day: "月" },
+        { day: "火", isActive },
+        { day: "水" },
+        { day: "木", isActive },
+        { day: "金" },
+        { day: "土" },
+    ];
+    return (
+        <div id="activity" className={styles.activity}>
+            <SectionTitle title="活動日時" enTitle="activity" />
+            <div className={styles.activityDays}>
+                {ActivityDays.map((day) => (
+                    <div
+                        key={day.day}
+                        className={`${styles.day} ${day.isActive ? styles.activeDay : ""}`}
+                    >
+                        {day.day}
+                    </div>
+                ))}
+            </div>
+            <div className={styles.activityTimes}>
+                <div className={styles.pie} />
+                <div className={styles.text}>
+                    <span>放課後</span>から<span>閉館</span>まで
+                    <br />
+                    活動しています！
+                </div>
+            </div>
+        </div>
+    );
 }
--- src/components/Members.tsx
+++ src/components/Members.tsx
@@ -5,150 +5,200 @@ import MembersGrade from "@/components/MembersGrade";
 import SectionTitle from "@/components/SectionTitle";
 
 const membersByGrade = {
-	2: [
-		{
-			name: "本田 有人",
-			kana: "ほんだ ゆうと",
-			course: "Webデザイン",
-			field: "engineer",
-			hobby: "ギターを弾くこと",
-			message: "ムキムキエンジニアになりたい💪💪",
-			image: "yhonda.png",
-		},
-		{
-			name: "平田 晃大",
-			kana: "ひらた こうだい",
-			course: "Webデザイン",
-			field: "engineer",
-			hobby: "歌を聴くこと・歌うこと",
-			message: "一緒にご飯でも行きましょ",
-			image: "khirata.jpg",
-		},
-		{
-			name: "川上 采華",
-			kana: "かわかみ あやか",
-			course: "Webデザイン",
-			field: "designer",
-			hobby: "ゲーム、ゲーム実況を見ること",
-			message: "みんな仲良しです！",
-			image: "akawakami.jpg",
-		},
-		{
-			name: "岡田 未希",
-			kana: "おかだ みき",
-			course: "Webデザイン",
-			field: "engineer",
-			hobby: "可愛いものを愛でる",
-			message: "みんなで楽しく頑張っていきましょう！",
-			image: "mokada.jpg",
-		},
-		{
-			name: "林 煜昭",
-			kana: "りん いくあき",
-			course: "Webデザイン",
-			field: "engineer",
-			hobby: "アイドル、バイク",
-			message: "一緒にWebクリエイターを目指して頑張りましょう！！",
-			image: "irin.jpg",
-		},
-		{
-			name: "ソンスヨン",
+    1: [{}],
+    2: [
+        {
+            name: "山形 海斗",
+            kana: "やまがた かいと",
+            course: "Webデザイン",
+            field: "engineer",
+            hobby: "バイクツーリング",
+            message: "気軽に話しかけてください！",
+            image: "yamagata.webp",
+        },
+        {
+            name: "高木 湊二郎",
+            kana: "たかぎ そうじろう",
+            course: "Webデザイン",
+            field: "engineer",
+            hobby: "勉強、カラオケ",
+            message: "コーディングならまかせろ",
+            image: "takagi.webp",
+        },
+        {
+            name: "金谷 魁星",
+            kana: "かなや かいせい",
+            course: "Webデザイン",
+            field: "engineer",
+            hobby: "音楽、グルメ",
+            message: "たくさんおしゃべりしましょう！",
+            image: "kanaya.webp",
+        },
+        {
+            name: "友塚 亮羽",
+            kana: "ともずか りょう",
+            course: "Webデザイン",
+            field: "engineer",
+            hobby: "体を動かすこと、読書",
+            message: "シャイなので気軽に話しかけてください！",
+            image: "tomozuka.webp",
+        },
+        {
+            name: "遠藤 来夏",
+            kana: "えんどう こなつ",
+            course: "Webデザイン",
+            field: "engineer",
+            hobby: "洋画, 洋楽, 少年漫画",
+            message: "enjoy~！",
+            image: "kendo.webp",
+        },
+        {
+            name: "菅野 友香",
+            kana: "かんの ともか",
+            course: "Webデザイン",
+            field: "designer",
+            hobby: "イラスト、手芸",
+            message: "ここは楽しいサークルだよ！",
+            image: "kanno.webp",
+        },
+        {
+            name: "守安 葉子",
+            kana: "もりやす はこ",
+            course: "Webデザイン",
+            field: "designer",
+            hobby: "",
+            message: "",
+            image: "unnamed.png",
+        },
+        {
+            name: "波戸 ",
+            kana: "はと ",
+            course: "IT開発エキスパート",
+            field: "engineer",
+            hobby: "",
+            message: "",
+            image: "unnamed.png",
+        },
+    ],
+    3: [
+        {
+            name: "本田 有人",
+            kana: "ほんだ ゆうと",
+            course: "Webデザイン",
+            field: "engineer",
+            hobby: "ギターを弾くこと",
+            message: "ムキムキエンジニアになりたい💪💪",
+            position: "部長",
+            image: "yhonda.png",
+        },
+        {
+            name: "平田 晃大",
+            kana: "ひらた こうだい",
+            course: "Webデザイン",
+            field: "engineer",
+            hobby: "歌を聴くこと・歌うこと",
+            message: "一緒にご飯でも行きましょ",
+            position: "副部長",
+            image: "khirata.jpg",
+        },
+        {
+            name: "川上 采華",
+            kana: "かわかみ あやか",
+            course: "Webデザイン",
+            field: "designer",
+            hobby: "ゲーム、ゲーム実況を見ること",
+            message: "みんな仲良しです！",
+            position: "副部長",
+            image: "akawakami.jpg",
+        },
+        {
+            name: "岡田 未希",
+            kana: "おかだ みき",
+            course: "Webデザイン",
+            field: "engineer",
+            hobby: "可愛いものを愛でる",
+            message: "みんなで楽しく頑張っていきましょう！",
+            image: "mokada.jpg",
+        },
+        {
+            name: "林 煜昭",
+            kana: "りん いくあき",
+            course: "Webデザイン",
+            field: "engineer",
+            hobby: "アイドル、バイク",
+            message: "一緒にWebクリエイターを目指して頑張りましょう！！",
+            image: "irin.jpg",
+        },
+        {
+            name: "ソンスヨン",
 
-			course: "Webデザイン",
-			field: "designer",
-			hobby: "料理",
-			message: "作品を作るのは楽しいですよね!",
-			image: "sson.jpg",
-		},
-		{
-			name: "砂原 彩花",
-			kana: "すなはら あやか",
-			course: "Webデザイン",
-			field: "engineer",
-			hobby: "洋楽を聴くこと",
-			message: "コーディング難しいけど楽しいでーす！",
-			image: "asunahara.jpg",
-		},
-		{
-			name: "グエンドゥックフィン",
-			course: "Webデザイン",
-			field: "engineer",
-			hobby: "音楽、アニメ",
-			message: "コーディング頑張りみゃす！！💪💪",
-			image: "fnguyen.jpg",
-		},
-		{
-			name: "上森 拓翔",
-			kana: "うえもり たくと",
-			course: "Webデザイン",
-			field: "engineer",
-			hobby: "写真撮ること",
-			message: "コーディング一緒に頑張りましょう！",
-			image: "tuemori.jpg",
-		},
-	],
-	3: [
-		{
-			name: "大垣 陸斗",
-			kana: "おおがき りくと",
-			course: "Webデザイン",
-			field: "engineer",
-			hobby: "おいしいご飯を食べに行く",
-			message: "みんなで楽しく頑張っていきましょう！",
-			position: "部長",
-			image: "roogaki.jpg",
-		},
-		{
-			name: "細田 海斗",
-			kana: "ほそだ かいと",
-			course: "Webデザイン",
-			field: "designer",
-			hobby: "歌を聴くこと・歌うこと",
-			position: "副部長",
-			message: "みんなで切磋琢磨し合い助け合いながらがんばってまーす！",
-			image: "khosoda.jpg",
-		},
-		{
-			name: "福村 晃弘",
-			kana: "ふくむら あきひろ",
-			course: "Webデザイン",
-
-			field: "director",
-			hobby: "おいしいご飯を食べに行く",
-			position: "副部長",
-			message: "みんなで楽しく頑張っていきましょう！",
-			image: "ahukumura.jpg",
-		},
-		{
-			name: "松岡 優輝",
-			kana: "まつおか ゆうき",
-			course: "IT開発エキスパート",
-			field: "engineer",
-			hobby: "ダーツ",
-			message: "一緒に作品作って成長しましょう^^*",
-			image: "ymatsuoka.jpeg",
-		},
-	],
+            course: "Webデザイン",
+            field: "designer",
+            hobby: "料理",
+            message: "作品を作るのは楽しいですよね!",
+            image: "sson.jpg",
+        },
+        {
+            name: "グエンドゥックフィン",
+            course: "Webデザイン",
+            field: "engineer",
+            hobby: "音楽、アニメ",
+            message: "コーディング頑張りみゃす！！💪💪",
+            image: "fnguyen.jpg",
+        },
+        {
+            name: "上森 拓翔",
+            kana: "うえもり たくと",
+            course: "Webデザイン",
+            field: "engineer",
+            hobby: "写真撮ること",
+            message: "コーディング一緒に頑張りましょう！",
+            image: "tuemori.webp",
+        },
+    ],
+    4: [
+        {
+            name: "松岡 優輝",
+            kana: "まつおか ゆうき",
+            course: "IT開発エキスパート",
+            field: "engineer",
+            hobby: "ダーツ",
+            message: "一緒に作品作って成長しましょう^^*",
+            image: "ymatsuoka.jpeg",
+        },
+    ],
 };
 
 export default function Members() {
-	return (
-		<div id="members" className={styles.members}>
-			<SectionTitle title="メンバー紹介" enTitle="Members" />
-			<div className={styles.members}>
-				<MembersGrade grade={3} />
-				<div className={styles.grade}>
-					{membersByGrade[3].map((member) => (
-						<Member key={member.name} {...member} />
-					))}
-				</div>
-				<MembersGrade grade={2} />
-				<div className={styles.grade}>
-					{membersByGrade[2].map((member) => (
-						<Member key={member.name} {...member} />
-					))}
-				</div>
-			</div>
-		</div>
-	);
+    return (
+        <div id="members" className={styles.members}>
+            <SectionTitle title="メンバー紹介" enTitle="Members" />
+            <div className={styles.members}>
+                <MembersGrade grade={4} />
+                <div className={styles.grade}>
+                    {membersByGrade[4].map((member) => (
+                        <Member key={member.name} {...member} />
+                    ))}
+                </div>
+                <MembersGrade grade={3} />
+                <div className={styles.grade}>
+                    {membersByGrade[3].map((member) => (
+                        <Member key={member.name} {...member} />
+                    ))}
+                </div>
+                <MembersGrade grade={2} />
+                <div className={styles.grade}>
+                    {membersByGrade[2].map((member) => (
+                        <Member key={member.name} {...member} />
+                    ))}
+                </div>
+                {/* <MembersGrade grade={1} />
+                <div className={styles.grade}>
+                    {membersByGrade[1].map((member) => (
+                        <Member key={member.name} {...member} />
+                    ))}
+                </div> */}
+            </div>
+        </div>
+    );
 }

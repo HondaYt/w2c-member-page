db3987c0d3227393ef7ba48232bedcf819d8cdcd
koudaihirata <2230051@ecc.ac.jp>
2025-04-21 12:40:47 +0900

    活動技修正

--- src/components/MainVisual.tsx
+++ src/components/MainVisual.tsx
@@ -4,23 +4,23 @@ import Link from "next/link";
 import styles from "@/styles/components/main_visual.module.scss";
 
 export default function MainVisual({
-	children,
+    children,
 }: {
-	children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-	return (
-		<div id="mainVisual" className={styles.mainVisual}>
-			<div className={styles.mainVisualText}>
-				<h2 className={styles.mainVisualTextTitle}>
-					毎週、月火木の
-					<br />
-					放課後活動中！！！
-				</h2>
-				<p className={styles.mainVisualTextLink}>
-					<a href="https://w2c-form.vercel.app/">入部はこちらから</a>
-				</p>
-			</div>
-			{children}
-		</div>
-	);
+    return (
+        <div id="mainVisual" className={styles.mainVisual}>
+            <div className={styles.mainVisualText}>
+                <h2 className={styles.mainVisualTextTitle}>
+                    毎週、火木の
+                    <br />
+                    放課後活動中！！！
+                </h2>
+                <p className={styles.mainVisualTextLink}>
+                    <a href="https://w2c-form.vercel.app/">入部はこちらから</a>
+                </p>
+            </div>
+            {children}
+        </div>
+    );
 }
